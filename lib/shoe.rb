43285ed27ba0bde03f0c6a9c5b54c779777059ca
adeolaayedun <deola.ayedun@gmail.com>
2018-06-07 18:47:38 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,67 @@
-# Make your shoe class here!
+#shoe.rb
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def maker=(maker)
+    @maker = maker
+  end
+
+  def maker
+    @maker
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def size
+    @size
+  end
+
+  def material=(material)
+    @material = material
+  end
+
+  def material
+    @material
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def condition
+    @condition
+  end
+
+  def cobble=(cobble)
+    @cobble = cobble
+  end
+
+  def cobble
+    @cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
+
+#puts("Your shoe is as good as new!")
+
+Shoe.new("Nike")
+
+
+#learn spec/02_shoe_spec.rb
